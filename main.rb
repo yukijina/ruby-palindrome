d1b12b470223655f8298511d9b1b2db7bb0a75ee
Yukiji Nakatsuka <yukijina@gmail.com>
2018-12-01 22:07:51 -0800

    corrected typo

--- main.rb
+++ main.rb
@@ -3,15 +3,15 @@ string2 = "abskksba"
 string3 = "abyyuhughue"
 string4 = "jhhhuuhhhj"
 string5 = "jkhhhkj"
-string6 = "Yo banana boy"
+string6 = "Yo banana boy!"
 string7 = "UFO tofu"
 
-#We want to find palindrom
+#We want to find palindrome
 #Bonus  - Also characters should not be duplicate
 
 def find_palindrome(string)
   #\s, space and comma. \! exclamation mark. \- hyphen
-  #replace to "" meaning no space, comma, ! and hypen
+  #replace to "" meaning no space, comma, ! and hyphen
   str = string.gsub(/[\s,\!\-]/ ,"").downcase
 
   #divide string to two and compare first and second half of string.
@@ -23,18 +23,18 @@ def find_palindrome(string)
     second_half_str = str.slice(median, str.length)
     revised_second_half_str = second_half_str.reverse
 
-    #change strig to array and split to charactar to compare if there are duplicate charactar (we use uniq to get unique charactar)
+    #change strig to array and split to character to compare if there are duplicate character (we use uniq to get unique character)
     arr1 = first_half_str.split("").to_a
     arr2 = first_half_str.split("").to_a.uniq
 
-    #check if first_half_str & second_half_str have same charactor.
-    #if charactar is duplicate, output error message
+    #check if first_half_str & second_half_str have same character.
+    #if character is duplicate, output error message
 
   #when string.length is even number
   if str.length.even?
     if first_half_str == revised_second_half_str
         if arr1 != arr2
-          puts "#{string} has more than two same charactors!"
+          puts "#{string} has more than two same characters!"
         else
           puts "Yes, #{string} is a palindrome."
         end
@@ -47,14 +47,14 @@ def find_palindrome(string)
 
   #when string is odd. *if odd, string.length is rounddown
   elsif str.length.odd?
-    #get last charactar  *str[start, length]. -1 means last charactar
+    #get last character  *str[start, length]. -1 means last character
     last_char = revised_second_half_str[-1, 1]
-    #remove last charactar
+    #remove last character
     second = revised_second_half_str.chomp(last_char)
-    #now compare if first half and new second half has same charactar
+    #now compare if first half and new second half has same character
     if first_half_str == second
         if arr1 != arr2
-          puts "#{string} has more than two same charactors!"
+          puts "#{string} has more than two same characters!"
         else
           puts "Yes, #{string} is a palindrome."
         end
